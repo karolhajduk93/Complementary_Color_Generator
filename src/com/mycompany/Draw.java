6b7381a8969ad7cec1f6964aebdd6eb9939da5ea
Karol <karolhajduk93@gmail.com>
2018-07-22 16:41:50 +0200

    Choosing and displaying color

---
+++ src/com/mycompany/Draw.java
@@ -0,0 +1,25 @@
+package com.mycompany;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Draw extends JComponent {
+
+    Color color;
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setPaint(color);
+        g2.drawRect(20, 20, 100, 100);
+        g2.fillRect(20, 20, 100, 100);
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Draw(Color color){
+        this.color = color;
+    }
+}
