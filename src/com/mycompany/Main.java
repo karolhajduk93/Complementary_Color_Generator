0a75392bf156cc3941bbb76d845ca84e3ec3758d
Karol <karolhajduk93@gmail.com>
2018-07-22 16:41:13 +0200

    Choosing and displaying color

--- src/com/mycompany/Main.java
+++ src/com/mycompany/Main.java
@@ -1,9 +1,16 @@
 package com.mycompany;
 
 import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
 
 public class Main extends JFrame{
-    
+
+    JColorChooser colorChooser;
+    JPanel panel;
+    Draw draw;
+    Color color;
+
     public static void main(String[] args) {
         new Main();
     }
@@ -15,6 +22,23 @@ public class Main extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Complementary color");
 
+        panel = new JPanel();
+        draw = new Draw(color);
+        colorChooser = new JColorChooser(Color.BLACK);
+        colorChooser.getSelectionModel().addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                color = colorChooser.getColor();
+                draw.setColor(color);
+                draw.revalidate();
+                draw.repaint();
+            }
+        });
+        panel.add(colorChooser);
+
+
+        this.add(draw, BorderLayout.CENTER);
+        this.add(panel, BorderLayout.PAGE_END);
         this.setVisible(true);
     }
 }
